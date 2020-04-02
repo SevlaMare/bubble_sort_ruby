aed7473a7151babfc88d72d4f07280594ac5f0d0
SevlaMare <sevilamare@gmail.com>
2020-04-02 17:57:40 -0300

    bubble_sort_by first draft

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,3 @@
-l1 = [4, 3, 78, 2, 0, 2]
-
 def bubble_sort(array)
   trigger = true
   while trigger
@@ -9,13 +7,13 @@ def bubble_sort(array)
     trigger = false
 
     # each lap do (n-1)
-    # because it's already sorted
+    # because last element it's already sorted
     n = 1
     while i < array.length - n
       if array[i] > array[i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
 
-        # if one swap in the whole lap
+        # if one swap in the whole lap, start again
         trigger = true
       end
       i += 1
@@ -25,4 +23,36 @@ def bubble_sort(array)
   array
 end
 
-p bubble_sort(l1)
+# test 1
+p bubble_sort([4, 3, 78, 2, 0, 2])
+
+
+
+
+def bubble_sort_by(array)
+  for i in array
+    left = array[i]
+    right = array[i + 1]
+    pair = yield(left, right)
+
+    if pair < 0
+      # sorted, left is smaller
+      return -1
+    elsif pair > 0
+      # not sorted, right is smaller
+      return 1
+    elsif pair == 0
+      # nothing to do, they are equals
+      return 0
+    end
+  end
+
+end
+
+
+
+# test 2
+p bubble_sort_by(["hi","hello","hey"]) do |left, right|
+  left.length - right.length
+end
+# ["hi", "hey", "hello"]
