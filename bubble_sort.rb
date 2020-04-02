8a8831b61ff598e7cb48c4a00f745c5140ed1a64
SevlaMare <sevilamare@gmail.com>
2020-04-02 14:52:09 -0300

    optimized

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,15 +7,19 @@ while trigger
   # if no swap, so is already sorted
   trigger = false
 
-  while i < l1.length - 1
+  # each lap do (n-1)
+  # because it's already sorted
+  n = 1
+  while i < l1.length - n
     if l1[i] > l1[i + 1]
       l1[i], l1[i + 1] = l1[i + 1], l1[i]
 
       # if one swap in the whole lap
       trigger = true
     end
-
     i += 1
   end
-  p l1 # final form after lap
+  n -= 1
 end
+
+p l1
