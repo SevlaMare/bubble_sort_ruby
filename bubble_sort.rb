d79795798ab678f6f55b19f71f54ff21fbf67703
SevlaMare <sevilamare@gmail.com>
2020-04-02 14:30:48 -0300

    loop until sort all array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,21 @@
 l1 = [4, 3, 78, 2, 0, 2]
-# l2 = [0,2,2,3,4,78] # sorted
 
-# trigger = true
-# while trigger
-#   for i in (0...l1.length-1)
-#     p i
-#   end
-#   p "finish lap"
-# end
+trigger = true
+while trigger
+  i = 0
 
-# l1 = [4,3,78,2,0,2]
-# l1 = [3, 4, 2, 0, 2, 78]
-# l1 = [3, 2, 0, 2, 4, 78]
-# l1 = [2, 0, 2, 3, 4, 78]
+  # if no swap, so is already sorted
+  trigger = false
 
-i = 0
-while i < l1.length - 1
-  p l1[i]
-  l1[i], l1[i + 1] = l1[i + 1], l1[i] if l1[i] > l1[i + 1]
-  i += 1
+  while i < l1.length - 1
+    if l1[i] > l1[i + 1]
+      l1[i], l1[i + 1] = l1[i + 1], l1[i]
+
+      # if one swap in the whole lap
+      trigger = true
+    end
+
+    i += 1
+  end
+  p l1 # final form after lap
 end
-
-p l1
