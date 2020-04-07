c24b2d3ad67b8fe8b8b143ed46862746f6db7066
SevlaMare <sevilamare@gmail.com>
2020-04-07 16:41:14 -0300

    sort by fixed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,22 +1,19 @@
 def bubble_sort(array)
   trigger = true
   while trigger
-    i = 0
-
     # if no swap, so is already sorted
     trigger = false
 
     # each lap do (n-1)
     # because last element it's already sorted
     n = 1
-    while i < array.length - n
+    (0...array.length - n).each do |i|
       if array[i] > array[i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
 
         # if one swap in the whole lap, start again
         trigger = true
       end
-      i += 1
     end
     n -= 1
   end
@@ -26,19 +23,26 @@ end
 p bubble_sort([4, 3, 78, 2, 0, 2])
 
 
-def bubble_sort_by(arx)
-  (0...arx.length - 1).each do |i|
-    result = yield(arx[i], arx[i - 1])
-    if result.positive?
-      arx[i], arx[i + 1] = arx[i + 1], arx[i]
-    end
-  end
-  arx
-end
+def bubble_sort_by(array)
+  trigger = true
+  while trigger
+    trigger = false
 
+    n = 1
+    (0...array.length - n).each do |i|
+      if yield(array[i], array[i + 1]).to_i.positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+
+        trigger = true
+      end
+    end
+    n -= 1
+  end
+  array
+end
 
 
 
 p bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
 p bubble_sort_by(%w[hi hello hey by]) { |left, right| left.length - right.length }
-#p bubble_sort_by(%w[b a aaa aa aaa a aa]) { |left, right| left.length - right.length }
+p bubble_sort_by(%w[b a aaa aa aaa a aa]) { |left, right| left.length - right.length }
