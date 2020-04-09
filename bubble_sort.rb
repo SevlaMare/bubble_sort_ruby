0a90cc8ed4e413627b75bad3652193fc870e8a02
SevlaMare <sevilamare@gmail.com>
2020-04-09 11:49:15 -0300

    fix n count

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,13 @@
 def bubble_sort(array)
   trigger = true
+  n = 1
   while trigger
     # if no swap, so is already sorted
     trigger = false
 
     # each lap do (n-1)
     # because last element it's already sorted
-    n = 1
+
     (0...array.length - n).each do |i|
       next unless array[i] > array[i + 1]
 
@@ -15,17 +16,17 @@ def bubble_sort(array)
       # if one swap in the whole lap, start again
       trigger = true
     end
-    n -= 1
+    n += 1
   end
   array
 end
 
 def bubble_sort_by(array)
   trigger = true
+  n = 1
   while trigger
     trigger = false
 
-    n = 1
     (0...array.length - n).each do |i|
       next unless yield(array[i], array[i + 1]).positive?
 
@@ -33,7 +34,14 @@ def bubble_sort_by(array)
 
       trigger = true
     end
-    n -= 1
+    n += 1
   end
   array
 end
+
+
+p bubble_sort([3,1,2])
+p bubble_sort([1,-1,0])
+p bubble_sort([-1,-3,-4,-6,-7])
+
+#p bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
